1f50b8a1b96c20b7a711b1c51b33199601b3f199
Riley Densmore <111328781+Rileyed2006@users.noreply.github.com>
2023-03-24 22:10:44 +0000

    Fri, Mar 24, 2023, 3:10 PM -07:00

--- js/typing.js
+++ js/typing.js
@@ -1,6 +1,6 @@
 const textEl =document.getElementById('text')
 const speedEl = document.getElementById('speed')
-const text = "Naomi Works At WildWaters <3"
+const text = "Go to Dell Taco,  They've got fre freesha freesha vacado,  freeshavacado!!!  <3"
 let idx = 1
 let speed = 300
 
