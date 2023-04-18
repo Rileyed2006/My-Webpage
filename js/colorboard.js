f9fe1a6e75ba2ccc1eedefe14fc74ae354a5cf90
Riley Densmore <111328781+Rileyed2006@users.noreply.github.com>
2023-04-18 04:59:08 +0000

    Mon, Apr 17, 2023, 9:59 PM -07:00

---
+++ js/colorboard.js
@@ -0,0 +1,9 @@
+const container = document.getElementById('container')
+const colors = ['#e74526', '#265adf', #24a123', '#24516ff']
+const SQUARES = 500
+
+for (Let i = 0; i < SQUARES; i++){
+    const square = document.createElement('div')
+    square.classList.add('square')
+    
+}
