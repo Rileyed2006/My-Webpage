845913c66ae0019aec16454d5e486cae5d006a64
Riley Densmore <111328781+Rileyed2006@users.noreply.github.com>
2023-05-23 22:54:51 +0000

    Tue, May 23, 2023, 3:54 PM -07:00

--- js/progress.js
+++ js/progress.js
@@ -1,12 +1,16 @@
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const progress = document.getElementById('progress')
-const circles = document.getElementById('.circle')
+const circles = document.querySelectorAll('.circle')
 
 
 let currentActive = 1
 
-next.addEventListener('cick', () =>{
+
+
+next.addEventListener('click', () => {
+
+
     currentActive++
     if (currentActive > 4){
         currentActive = 4
@@ -16,7 +20,7 @@ next.addEventListener('cick', () =>{
 prev.addEventListener('click', () => {
     currentActive--
     if (currentActive < 1){
-        currentActive < 1
+        currentActive = 1
     }
     update()
 })
@@ -34,9 +38,13 @@ function update(){
         prev.disabled = false
         next.disabled = false
     }
+
+    const actives = document.querySelectorAll('.active')
+    progress.style.width = 1 / 3 * 100 + '%'
+
     circles.forEach((circle, index) => {
         if (index < currentActive){
-            circle.calssList.add('active')
+            circle.classList.add('active')
         }
         else{
             circle.classList.remove('active')
